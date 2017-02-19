b465a56a661941dc1125701fa0791ae4917756f4
Gabe Lockner <gblockner@dmacc.edu>
2017-02-19 13:25:35 -0600

    Add files via upload

---
+++ Character.cpp
@@ -0,0 +1,51 @@
+// Implementation file for the Monster class
+// last updated Wednesday February 15, 2017
+
+#include <iostream>
+#include <string>
+
+#include "Character.h"
+
+Character::Character(int level, std::string name)
+{
+ nm = name;
+ lvl = level;
+ hp = level * 5 + 10;
+ atk = level * 2.1;
+ def = level * 1.5;
+}
+
+void Character::getStats()
+{
+ std::cout << "\nRetrieving Stats...\n";
+ std::cout << "NAME:    " << nm << std::endl;
+ std::cout << "LEVEL:   " << lvl << std::endl;
+ std::cout << "HP:      " << hp << std::endl;
+ std::cout << "ATTACK:  " << atk << std::endl;
+ std::cout << "DEFENSE: " << def << std::endl;
+}
+
+int Character::Attack()
+{
+ return atk;
+}
+
+int Character::GetHP()
+{
+ return hp;
+}
+
+std::string Character::GetName()
+{
+ return nm;
+}
+
+bool Character::Hit(int dmg)
+{
+ hp = hp - dmg;
+}
+
+bool Character::Run()
+{
+ return true;
+}
---
+++ Creature.h
@@ -0,0 +1,23 @@
+#ifndef CREATURE_H
+#define CREATURE_H
+
+// ##########################
+// Creature class declaration
+// ##########################
+
+class Creature
+{
+ private:
+  int lvl, atk, def, hp;
+  std::string nm;
+ public:
+  Creature(int, std::string);
+  void getStats();
+  int Attack();
+  void Hit(int);
+  bool Run();
+  int GetHP();
+  std::string GetName();
+};
+
+#endif
