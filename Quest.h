ed57da1b16cc94e5102d9299bd1e15b543ec5726
Hydroplane72 <mrrtrombone@gmail.com>
2017-02-26 16:37:43 -0600

    Added some prototypes to header

--- Quest.h
+++ Quest.h
@@ -4,14 +4,19 @@
 class Quest
 {
 	private:
-		int questNum;	
+		int questNum;
+		//ProtoTypes
+		int rand(int min, int max); //Random Number Generator
+		std::string getScene(int,int); //gets the quest scene info
+		bool monsterCheck(int); //Simulates checking for monsters
+		
+
 	public:
 		Quest();
 		//gets the quest scene info
 		int startQuest(int);
 		bool Battle(std::string monsterName,int monsterLevel, int monsterAttack, int monsterHealth, int playerAttack, int playerHealth);
-	
+		
 };
 
 #endif
-
